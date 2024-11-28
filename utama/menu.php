54ae5868b163b231329d44095a4c55d6d0e6b637
Henhen321 <henhendrazat@gmail.com>
2024-11-28 20:36:57 +0700

    first commit

--- utama/menu.php
+++ utama/menu.php
@@ -1,3 +1,13 @@
+<?php
+session_start(); // Memulai sesi
+
+// Cek apakah pengguna sudah login
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php"); // Jika belum login, arahkan ke halaman login
+    exit();
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
